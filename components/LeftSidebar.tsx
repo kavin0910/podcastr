182bc39888e55c694bdce06c273d9148cee20734
Muskan Mittal <mittalmuskan758@gmail.com>
2024-06-08 21:33:36 +0530

    updated code

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import Image from "next/image";
 import { sidebarLinks } from "@/constants";
 import { usePathname, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const LeftSidebar = () => {
     const pathname = usePathname()
@@ -22,7 +23,9 @@ const LeftSidebar = () => {
             const isActive = pathname === route || pathname.startsWith
             (`${route}`);
 
-            return <Link href={route} key={label} className="flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start">
+            return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+                'bg-nav-focus border-r-4 border-orange-1' : isActive
+            })}>
                 <Image src={imgURL} alt={label} width={24} height={24} />
                 <p>{label}</p>
             </Link>
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <h1 className='right_sidebar text-white-1'>RightSidebar</h1>
+  )
+}
+
+export default RightSidebar
