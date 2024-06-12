87ecb8fff289d4afcf8a26f0bda2942c4344f15a
Muskan Mittal <mittalmuskan758@gmail.com>
2024-06-12 22:50:12 +0530

    updated code

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,24 +1,23 @@
 import { GeneratePodcastProps } from '@/types'
 import React, { useState } from 'react'
-import { Label } from './ui/label'
-import { Textarea } from './ui/textarea'
-import { Button } from './ui/button'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { Button } from '@/components/ui/button'
 import { Loader } from 'lucide-react'
 import { useAction, useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { v4 as uuidv4 } from 'uuid';
-import { useToast } from './ui/use-toast'
+import { useToast } from "@/components/ui/use-toast"
 
-import { useUploadFiles } from '@xixixao/uploadstuff/react'
+import { useUploadFiles } from '@xixixao/uploadstuff/react';
 
 const useGeneratePodcast = ({
   setAudio, voiceType, voicePrompt, setAudioStorageId
 }: GeneratePodcastProps) => {
-  // Logic for podcast generation
   const [isGenerating, setIsGenerating] = useState(false);
-  const { toast } = useToast();
+  const { toast } = useToast()
 
-  const generateUploadUrl = useMutation(api.files.generateUploadUrl)
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
   const { startUpload } = useUploadFiles(generateUploadUrl)
 
   const getPodcastAudio = useAction(api.openai.generateAudioAction)
@@ -29,7 +28,7 @@ const useGeneratePodcast = ({
     setIsGenerating(true);
     setAudio('');
 
-    if (!voicePrompt) {
+    if(!voicePrompt) {
       toast({
         title: "Please provide a voiceType to generate a podcast",
       })
@@ -43,7 +42,7 @@ const useGeneratePodcast = ({
       })
 
       const blob = new Blob([response], { type: 'audio/mpeg' });
-      const fileName = `podcast-${uuidv4()}.mp3`
+      const fileName = `podcast-${uuidv4()}.mp3`;
       const file = new File([blob], fileName, { type: 'audio/mpeg' });
 
       const uploaded = await startUpload([file]);
@@ -57,15 +56,15 @@ const useGeneratePodcast = ({
       toast({
         title: "Podcast generated successfully",
       })
-
     } catch (error) {
-      console.log('Error generating podcast', error);
+      console.log('Error generating podcast', error)
       toast({
         title: "Error creating a podcast",
         variant: 'destructive',
       })
       setIsGenerating(false);
     }
+    
   }
 
   return { isGenerating, generatePodcast }
@@ -73,43 +72,39 @@ const useGeneratePodcast = ({
 
 const GeneratePodcast = (props: GeneratePodcastProps) => {
   const { isGenerating, generatePodcast } = useGeneratePodcast(props);
+
   return (
     <div>
-      <div className='flex flex-col gap-2.5'>
-        <Label className='text-16 font-bold text-white-1'>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
           AI Prompt to generate Podcast
         </Label>
-        <Textarea
-          className="input-class foont-light focus-visible:ring-offset-orange-1"
+        <Textarea 
+          className="input-class font-light focus-visible:ring-offset-orange-1"
           placeholder='Provide text to generate audio'
           rows={5}
           value={props.voicePrompt}
           onChange={(e) => props.setVoicePrompt(e.target.value)}
         />
       </div>
-      <div className='mt-5 w-full max-w-[200px]'>
-        <Button
-          type="submit"
-          className="text-16 bg-orange-1 py-4 font-bold text-white-1"
-          onClick={generatePodcast}
-        >
-          {isGenerating ? (
-            <>
-              Generating
-              <Loader size={20} className="animate-pin mr-2" />
-            </>
-          ) : (
-            'Generate'
-          )}
-        </Button>
+      <div className="mt-5 w-full max-w-[200px]">
+      <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" onClick={generatePodcast}>
+        {isGenerating ? (
+          <>
+            Generating
+            <Loader size={20} className="animate-spin ml-2" />
+          </>
+        ) : (
+          'Generate'
+        )}
+      </Button>
       </div>
-
       {props.audio && (
-        <audio
+        <audio 
           controls
           src={props.audio}
           autoPlay
-          className='mt-5'
+          className="mt-5"
           onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)}
         />
       )}
