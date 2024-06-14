8fff4f5eb965859258036edef43470a46c1432d1
Muskan Mittal <mittalmuskan758@gmail.com>
2024-06-14 16:11:02 +0530

    updated code

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import Image from "next/image";
+import { PodcastCardProps } from "@/types";
+import { useRouter } from "next/navigation";
 
 const PodcastCard = ({
-  imgURL,
+  imgUrl,
   title,
   description,
   podcastId,
-}: {
-  imgURL: string;
-  title: string;
-  description: string;
-  podcastId: number;
-}) => {
+}: PodcastCardProps) => {
+  const router = useRouter();
+
+  const handleViews = () => {
+    // increase views
+
+    router.push(`/podcasts/${podcastId}`, {
+      scroll: true
+    })
+  }
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer" onClick={handleViews}>
       <figure className="flex flex-col gap-2">
         <Image
-          src={imgURL}
+          src={imgUrl}
           width={174}
           height={174}
           alt={title}
