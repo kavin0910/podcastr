f8cccbc692ab168bac2a5ff7afd9e3338701835e
Muskan Mittal <mittalmuskan758@gmail.com>
2024-06-14 12:28:06 +0530

    updated code

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button } from './ui/button'
 import { cn } from '@/lib/utils';
 import { Label } from './ui/label';
 import { Textarea } from './ui/textarea';
 import { GenerateThumbnailProps } from '@/types';
 import { Loader } from 'lucide-react';
+import { Input } from './ui/input';
+import Image from 'next/image';
 
 const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, setImagePrompt }: GenerateThumbnailProps) => {
   const [isAiThumbnail, setIsAiThumbnail] = useState(false);
-  const [isGenerating, setIsGenerating] = useState(false);
+  const [isImageLoading, setIsImageLoading] = useState(false);
+  const imageRef = useRef<HTMLInputElement>(null);
 
   const generateImage = async () => { }
 
@@ -28,22 +31,22 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
       </div>
 
       {isAiThumbnail ? (
-        <div>
+        <div className="flex flex-col gap-5">
           <div className="flex flex-col gap-2.5">
             <Label className="text-16 font-bold text-white-1">
-              AI Prompt to generate Podcast
+              AI Prompt to generate Thumbnail
             </Label>
             <Textarea
               className="input-class font-light focus-visible:ring-offset-orange-1"
-              placeholder='Provide text to generate audio'
+              placeholder='Provide text to generate thumbnail'
               rows={5}
               value={imagePrompt}
               onChange={(e) => setImagePrompt(e.target.value)}
             />
           </div>
-          <div className="mt-5 w-full max-w-[200px]">
+          <div className="w-full max-w-[200px]">
             <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" onClick={generateImage}>
-              {isGenerating ? (
+              {isImageLoading ? (
                 <>
                   Generating
                   <Loader size={20} className="animate-spin ml-2" />
@@ -55,10 +58,29 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
           </div>
         </div>
       ) : (
-        <div>
+        <div className='image_div' onClick={() => imageRef?.current?.click()}>
+          <Input
+            type='file'
+            className='hidden'
+            ref={imageRef}
+          />
 
+          {!isImageLoading ? (
+            <Image src="/icons/upload-image.svg" width={40} height={40} alt='upload' />
+          ) : (
+            <div className='text-16 flex-center font-medium text-white-1'>
+              Uploading
+              <Loader size={20} className="animate-spin ml-2" />
+            </div>
+          )}
+          <div className='flex flex-col items-center gap-1'>
+            <h2 className='text-12 font-bold text-orange-1'>
+              Click to upload
+            </h2>
+            <p className='text-12 font-normal text-gray-1'>SVG, PNG, JPG, OR gif (max. 1080X1080px)</p>
+          </div>
         </div>
-    )}
+      )}
     </>
   )
 }
