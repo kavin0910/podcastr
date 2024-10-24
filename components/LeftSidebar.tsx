a8a5e9218f6d9af47acdb6110e687ef03ab051cf
Muskan Mittal <mittalmuskan758@gmail.com>
2024-10-24 23:06:48 +0530

    removed comments

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import { sidebarLinks } from '@/constants'
-import { cn } from '@/lib/utils'
-import { SignedIn, SignedOut, useClerk } from '@clerk/nextjs';
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
-import React from 'react'
-import { Button } from './ui/button';
-import { useAudio } from '@/providers/AudioProvider';
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import { useAudio } from "@/providers/AudioProvider";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
@@ -17,37 +17,55 @@ const LeftSidebar = () => {
   const { audio } = useAudio();
 
   return (
-    <section className={cn("left_sidebar h-[calc(100vh-5px)]", {
-      'h-[calc(100vh-140px)]': audio?.audioUrl
-    })}>
+    <section
+      className={cn("left_sidebar h-[calc(100vh-5px)]", {
+        "h-[calc(100vh-140px)]": audio?.audioUrl,
+      })}
+    >
       <nav className="flex flex-col gap-6">
-        <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white max-lg:hidden">Podcastr</h1>
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Podcastr
+          </h1>
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
-          const isActive = pathname === route || pathname.startsWith(`${route}/`);
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
 
-          return (
-            // Conditionally render "Create Podcast" link only for signed-in users
-            (label === "Create Podcast" ? (
-              <SignedIn key={label}>
-                <Link href={route} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
-                  'bg-nav-focus border-r-4 border-orange-1': isActive
-                })}>
-                  <Image src={imgURL} alt={label} width={24} height={24} />
-                  <p>{label}</p>
-                </Link>
-              </SignedIn>
-            ) : (
-              <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
-                'bg-nav-focus border-r-4 border-orange-1': isActive
-              })}>
+          return label === "Create Podcast" ? (
+            <SignedIn key={label}>
+              <Link
+                href={route}
+                className={cn(
+                  "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                  {
+                    "bg-nav-focus border-r-4 border-orange-1": isActive,
+                  }
+                )}
+              >
                 <Image src={imgURL} alt={label} width={24} height={24} />
                 <p>{label}</p>
               </Link>
-            ))
+            </SignedIn>
+          ) : (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
           );
         })}
       </nav>
@@ -61,7 +79,10 @@ const LeftSidebar = () => {
       </SignedOut>
       <SignedIn>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button className="text-16 w-full bg-orange-1 font-extrabold" onClick={() => signOut(() => router.push('/'))}>
+          <Button
+            className="text-16 w-full bg-orange-1 font-extrabold"
+            onClick={() => signOut(() => router.push("/"))}
+          >
             Log Out
           </Button>
         </div>
